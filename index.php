e3ba31321404f517912824cccc895ee4675a5fcd
Chris <mtdbass5@gmail.com>
2013-04-27 21:52:41 -0400

    simplified code by using glob instead of scandir

--- index.php
+++ index.php
@@ -4,36 +4,29 @@
 		return $path_info['extension']; 
 	}
 	function get_contents($dir, $top_level = false) {
-		$ls = scandir($_SERVER['DOCUMENT_ROOT'] . $dir);
+		$search_base = $_SERVER['DOCUMENT_ROOT'] . $dir;
+		$ls = array_merge(
+			glob($search_base . '*', GLOB_ONLYDIR),
+			glob($search_base . '*.mp4', GLOB_NOSORT),
+			glob($search_base . '*.m4v', GLOB_NOSORT)
+		);
 		if ($top_level) {
 			echo '<ul>';
 		} else {
 			echo '<ul style="display: none;">';
 		}
-		for ($i = 2, $max = count($ls); $i < $max; $i += 1) {
-			$echo = false;
-			$name = $ls[$i];
+		for ($i = 0, $max = count($ls); $i < $max; $i += 1) {
+			$name = basename($ls[$i]);
 			$web_path = $dir . $name;
-			$abs_path = $_SERVER['DOCUMENT_ROOT'] . $web_path;
-			if (filetype($abs_path) === 'file') {
-				$class = get_ext($abs_path);
-			} else {
-				$class = 'dir';
-			}
-			if ($class === 'dir' || $class === 'mp4' || $class === 'm4v') {
-				$echo = true;
-				echo '<li>'; 
-				if ($class === 'dir') {
-					echo '<a class="dir" href="' . $web_path . '"><img src="/dev/media_lister/images/arrow_r.png" /></a>';
-					echo '<a href="' . $web_path . '">' . $name . '</a>';
-				} else {
-					echo '<img width="30" height="1" src="/dev/media_lister/images/s.gif" /><a class="' . $class . '" href="' . $web_path . '">' . $name . '</a>';
-				}
-			}
-			if ($class === 'dir') {
-				get_contents($web_path . '/');
-			} elseif ($echo) {
+			echo '<li>'; 
+			if (filetype($ls[$i]) === 'file') {
+				$class = get_ext($ls[$i]);
+				echo '<img width="30" height="1" src="/dev/media_lister/images/s.gif" /><a class="' . $class . '" href="' . $web_path . '">' . $name . '</a>';
 				echo '</li>';
+			} else {
+				echo '<img class="dir" src="/dev/media_lister/images/arrow_r.png" />';
+				echo '<a href="' . $web_path . '">' . $name . '</a>';
+				get_contents($web_path . '/');
 			}
 		}
 		echo '</ul>';
@@ -62,7 +55,7 @@
 			body { font-family: Helvetica, sans-serif; line-height: 1.7em; background-color: #333; color: #fff;}
 			ul {padding-left: 10px;}
 			li { list-style-type: none; }
-			a img { padding: 0 10px; }
+			img.dir { padding: 0 10px; cursor: pointer; }
 			span { color: #f00; }
 			a { text-decoration: none; color: #fff; }
 			a:hover { text-decoration: underline; }
@@ -81,19 +74,15 @@
 				function mod_src(o, n, that) {
 					that.setAttribute('src', that.getAttribute('src').replace(o, n));
 				}
-				$(document.body).on('click', 'a.dir', function () {
+				$(document.body).on('click', 'img.dir', function () {
 					var $_this = $(this),
-						arrow = $_this.children()[0],
 						$_sib_ul = $_this.siblings('ul');
-					if ($_sib_ul.children().length) {
-						if ($_sib_ul.is(':visible')) {
-							$_sib_ul.hide();
-							mod_src('_d', '_r', arrow);
-						} else {
-							$_sib_ul.show();
-							mod_src('_r', '_d', arrow);
-						}
-						return false;
+					if ($_sib_ul.is(':visible')) {
+						$_sib_ul.hide();
+						mod_src('_d', '_r', this);
+					} else {
+						$_sib_ul.show();
+						mod_src('_r', '_d', this);
 					}
 				});
 			}());
